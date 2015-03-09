28e4095e27a446041fb1a645a9fc04f7ead7ae25
Caterina <caterina.penone@gmail.com>
2015-03-09 16:59:10 -0300

    Added predtreeVar_pgls function

--- R/predtreeVar_pgls.R
+++ R/predtreeVar_pgls.R
@@ -1,32 +1,31 @@
 #' PGLS analysis accounting for variability in predictors and trees.
 #'
 #' \code{predtreeVar_pgls} performs \code{pgls} analysis accounting for variability in predictors and trees.
-#' It picks n times (\code {npred}) a random value within an interval for the predictor variable and 
-#' it repeats the analysis m times (\code {ntree}) for m trees in a multiPhylo file. 
+#' It picks n times a random value within an interval for the predictor variable and 
+#' it repeats the analysis m times for m trees in a \code{multiPhylo} file. 
 #' The output gives a mean and sd of the model parameters.
 #' @aliases predtreeVar_pgls
 #' @inheritParams ??inheritParams
 #' @param resp Numeric vector containing the response variable
 #' @param pred Vector containing the predictor variable
-#' @param se.pred A vector containing the standard error of \code{pred}  
+#' @param se.pred Vector containing the standard error of \code{pred}  
 #' @param tree A tree or list of tree of class \code{Phylo} or \code{multiPhylo}
 #' @param ntree If TRUE or class(tree)=multiPhylo, the number of times to repeat the analysis with n different 
 #' trees picked randomly in the multiPhylo file. If NULL, \code{ntree}=1
 #' @param npred If TRUE, the number of times to repeat the analysis generating a random value in the interval 
 #' [\code{pred}-\code{se.pred},\code{pred}+\code{se.pred}]. If NULL, \code{npred}=1
 #' @param method A character string indicating which method to use to generate a random value in the interval 
-#' [\code{pred}-\code{se.pred},\code{pred}+\code{se.pred}]. DEFAULT is normal distribution: "normal" 
+#' [\code{pred} - \code{se.pred}, \code{pred} + \code{se.pred}]. Default is normal distribution: "normal" 
 #' (function \code{\link{rnorm}}), uniform distribution is "uniform" (\code{\link{runif}})
 #' @param taxa.col A character vector of taxa names that will be used to match data rows to phylogeny tips.
-#' @param lambda A value for the lambda transformation. If NULL, (\code{lambda})="ML"
+#' @param lambda A value for the lambda transformation. If NULL, \code{lambda}="ML"
 #' @details This functions only works for simple linear regression \eqn{y = bx +
 #' a}. Future implementation will deal with more complex models.
 #' @return The function \code{predtreeVar_pgls} returns a list with the following
 #' components:
-#' @return \code{ model_results} Model parameters for each iteration
-#' @return \code{results} A data frame with all simulation estimates
+#' @return \code{model_results} Model parameters for each iteration
 #' @return \code{stats} Mean and sd of each parameter of the model
-#' @section Warning: This code is note fully checked. Please be aware.
+#' @section Warning: This code is not fully checked. Please be aware.
 #' If \code{ntree} and \code{npred} are set to 1, the function computes a classic pgls.
 #' @seealso \code{\link{pgls}}, \code{\link{comparative.data}}, \code{\link{runif}}, \code{\link{rnorm}}
 #' @examples
