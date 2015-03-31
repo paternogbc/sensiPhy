3b26ac350dd7bde75f522f25377e21f5ed40d271
Gustavo B Paterno <paternogbc@gmail.com>
2015-03-31 09:59:08 -0300

    minor

--- R/sensi_plot.R
+++ R/sensi_plot.R
@@ -23,9 +23,7 @@ sensi_plot <- function(x){
                               xlab("% of Species Removed ")+
                               geom_hline(yintercept=beta.0.low,linetype=2,color="red")+
                               geom_hline(yintercept=beta.0.up,linetype=2,color="red")+
-                              geom_hline(yintercept=beta.0,linetype=2,color="red",size=1.1)+
-                            theme(axis.title=element_text(size=16),
-                                  axis.text = element_text(size=14))
+                              geom_hline(yintercept=beta.0,linetype=2,color="red",size=1.1)
 
                     ## Mean estimated Betas:
                     med <- with(result,tapply(betas,n.removs,mean))
@@ -42,44 +40,39 @@ sensi_plot <- function(x){
                               xlab("Number of Species") + ylab("Mean Beta (+-SD)")+
                               geom_point(aes(x=nrow(x[[5]]),y=beta.0,size=3,colour="red"))+
                               theme(legend.position="none")+
-                              xlab("Number of species")+
-                            theme(axis.title=element_text(size=16),
-                                  axis.text = element_text(size=14))
+                              xlab("Number of species")
 
                     ## Power Analysis: p.value
-                    times <- table(result$n.removs)
+                    times <- table(result$n.removs)[1]
                     breaks <- unique(result$n.percents)
                     simu.sig <- result$p.values > .05
                     result$simu.sig <- simu.sig
-                    p.out <- (with(samp2$result,tapply(simu.sig,n.removs,sum))/times)
+                    reps <- table(result$n.removs)
+                    p.out <- (with(result,tapply(simu.sig,n.removs,sum))/reps)
                     power <- as.numeric(1-p.out)
                     power.tab <- data.frame(breaks,power)
                     p3 <-ggplot2::ggplot(power.tab,aes(y=power,x=breaks))+
-                              scale_y_continuous(limits=c(0,1),breaks=seq(0,1,.1))+
+                              scale_y_continuous(limits=c(0,1),breaks=seq(0,1,.05))+
                               scale_x_continuous(breaks=breaks)+
                               xlab("% Species removed")+
                               geom_point(size=5,colour="red")+
                               geom_line(colour="red")+
-                              ylab("Power  [p-value]")+
-                            theme(axis.title=element_text(size=16),
-                                  axis.text = element_text(size=14))
+                              ylab("Power  [p-value]")
 
                     ## Power Analysis: beta (percentage of betas > or < then CI)
                     beta.high <- result$betas > beta.0.up
                     beta.low <- result$betas < beta.0.low
                     result$beta.out.CI <- beta.high+beta.low
-                    b.out <-(with(result,tapply(beta.out.CI,n.removs,sum))/times)
+                    b.out <-(with(result,tapply(beta.out.CI,n.removs,sum))/reps)
                     power <- as.numeric(1-b.out)
                     power.tab <- data.frame(breaks,power)
                     p4 <- ggplot2::ggplot(power.tab,aes(y=power,x=breaks))+
-                              scale_y_continuous(limits=c(0,1),breaks=seq(0,1,.1))+
+                              scale_y_continuous(limits=c(0,1),breaks=seq(0,1,.05))+
                               scale_x_continuous(breaks=breaks)+
                               xlab("% Species removed")+
                               geom_point(size=5,colour="red")+
                               geom_line(colour="red")+
-                              ylab("% of correct estimated betas")+
-                            theme(axis.title=element_text(size=16),
-                                  axis.text = element_text(size=14))
+                              ylab("Power  [Beta]")
                     suppressWarnings(gridExtra::grid.arrange(p1,p2,p3,p4,ncol=2,nrow=2))
           }
           else      {
