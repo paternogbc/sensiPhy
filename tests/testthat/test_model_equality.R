bc08e4df86cfff65acf45d0b3f820f727751f397
Gustavo B Paterno <paternogbc@gmail.com>
2015-07-10 23:00:53 +0200

    included tests for: samp_xxx and influ_xxx

--- tests/testthat/test_model_equality.R
+++ tests/testthat/test_model_equality.R
@@ -21,6 +21,13 @@ test_that("mod.0 samp_phylolm is equal to phylolm", {
 
 ###: test samp_phyloglm:
 test_that("mod.0 samp_phyloglm is equal to phyloglm", {
+    set.seed(2468)
+    tree <- rtree(50)
+    pred<- rTraitCont(tree,root.value=0,sigma=1,model="BM")
+    cont_trait1 <- pred + rTraitCont(tree,model="BM",sigma=0.1)
+    bin_trait1<-rbinTrait(n=1,tree,beta=c(-1,0.5),alpha=0.1,
+                          X=cbind(rep(1,length(tree$tip.label)),pred))
+    dat<-data.frame(pred,cont_trait1,bin_trait1)
     original <- phyloglm(bin_trait1~pred,data = dat,phy = tree)
     mod.samp <- samp_phyloglm(bin_trait1~pred,data = dat,phy = tree,track = F,times = 1)
     expect_equal(summary.phyloglm(original)$coefficients,
@@ -29,6 +36,13 @@ test_that("mod.0 samp_phyloglm is equal to phyloglm", {
 
 ###: test influ_phylolm:
 test_that("mod.0 influ_phylolm is equal to phylolm", {
+    set.seed(2468)
+    tree <- rtree(50)
+    pred<- rTraitCont(tree,root.value=0,sigma=1,model="BM")
+    cont_trait1 <- pred + rTraitCont(tree,model="BM",sigma=0.1)
+    bin_trait1<-rbinTrait(n=1,tree,beta=c(-1,0.5),alpha=0.1,
+                          X=cbind(rep(1,length(tree$tip.label)),pred))
+    dat<-data.frame(pred,cont_trait1,bin_trait1)
     original <- phylolm(cont_trait1 ~ pred,phy = tree,data = dat,model = "lambda")
     mod.samp <- influ_phylolm(cont_trait1 ~ pred,phy = tree,data = dat,model = "lambda",
                              track = F,times = 1)
@@ -38,6 +52,13 @@ test_that("mod.0 influ_phylolm is equal to phylolm", {
 
 ###: test influ_phyloglm:
 test_that("mod.0 influ_phyloglm is equal to phyloglm", {
+    set.seed(2468)
+    tree <- rtree(50)
+    pred<- rTraitCont(tree,root.value=0,sigma=1,model="BM")
+    cont_trait1 <- pred + rTraitCont(tree,model="BM",sigma=0.1)
+    bin_trait1<-rbinTrait(n=1,tree,beta=c(-1,0.5),alpha=0.1,
+                          X=cbind(rep(1,length(tree$tip.label)),pred))
+    dat<-data.frame(pred,cont_trait1,bin_trait1)
     original <- phyloglm(bin_trait1~pred,data = dat,phy = tree)
     mod.samp <- influ_phyloglm(bin_trait1~pred,data = dat,phy = tree,track = F)
     expect_equal(summary.phyloglm(original)$coefficients,
