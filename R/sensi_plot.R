16966fcfb7143b7f43e42b28604d10ba749ea439
Gustavo B Paterno <paternogbc@gmail.com>
2015-07-24 11:05:22 +0200

    minor help file

--- R/sensi_plot.R
+++ R/sensi_plot.R
@@ -1,9 +1,10 @@
-#' Graphical sensitive analysis for comparative methods
+#' Graphical sensitivity analysis for comparative methods
 #'
 #' \code{sensi_plot} Plot results from \code{samp_influ_phyloglm},
 #' \code{influ_pgls}
 #' @aliases sensi_plot
-#' @param x output from \code{samp_gls}, \code{influ_gls}
+#' @param x output from \code{samp_phylolm}, \code{samp_phyloglm},
+#'  \code{influ_phylolm} and \code{influ_phyloglm}
 #' @param graphs choose which graphs should be printed on the output ("all", 1,2,3
 #' or 4)
 #' @param param choose which model parameter should be ploted  ("intercept" or "slope")
@@ -19,11 +20,11 @@ if (x[[1]] != "samp_phylolm" & x[[1]] != "samp_phyloglm" &
              samp_phylolm, influ_phylolm and influ_phyloglm")
 
 ### samp_phylolm or samp_phylolm output:
-        if (x[[1]] == "samp_phylolm" | x[[1]] == "samp_phyloglm")
-                plot_samp_phylolm(x,graphs,param)
+if (x[[1]] == "samp_phylolm" | x[[1]] == "samp_phyloglm")
+    plot_samp_phylolm(x,graphs,param)
 
 ### influ_phylolm or influ_phylolm output:
 if (x[[1]] == "influ_phylolm" | x[[1]] == "influ_phyloglm")
-        plot_influ_phylolm(x,graphs,param)
+    plot_influ_phylolm(x,graphs,param)
 }
 
