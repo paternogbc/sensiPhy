34bb68908f8e71bd72b6f7e647e6ae76f58f6ca0
caterinap <caterina.penone@gmail.com>
2015-08-06 13:59:30 +0200

    added match_dataphy to match species names prior to analysis

---
+++ R/match_dataphy.R
@@ -0,0 +1,46 @@
+match_dataphy <- function(data,phy){
+
+#Remove NA's before matching data and tips
+  if (sum(is.na(data))!=0 || sum(is.na(data))!=0)
+  {data<-data[!is.na(data$resp),]
+  data<-data[!is.na(data$pred),]
+  warning("NA's in response or predictor, rows with NA's were removed")}
+
+#Match data and phylogeny in comparative.data style
+if(inherits(phy, "multiPhylo")){  
+  phy1<-phy[[1]]}
+else
+  phy1<-phy
+
+tiplabl<-phy1$tip.label
+taxa.nam<-as.character(rownames(data))
+
+in.both <- intersect(taxa.nam, tiplabl)
+
+if (length(in.both) == 0)
+  stop("No tips are common to the dataset and phylogeny")
+
+mismatch<-union(setdiff(tiplabl,taxa.nam),setdiff(taxa.nam,tiplabl))
+if (length(mismatch) != 0)   warning("Phylogeny tips do not match the species list,
+                                     species were dropped from phylogeny or species list")
+
+#Drop species from tree
+phy<-lapply(phy,ape::drop.tip,tip=mismatch)
+class(phy)<-"multiPhylo"
+
+#transform NA's in SE columns into zeros
+#data[is.na(data)] <- 0
+
+#Reorder rows according to tip labels
+if(inherits(phy, "multiPhylo")){  
+  phy1<-phy[[1]]}
+else
+  phy1<-phy
+
+rownames(data)<-data$taxa.nam
+tip.order <- match(phy1$tip.label, rownames(data))
+if (any(is.na(tip.order)))
+  stop("Problem with sorting data frame: mismatch between tip labels and data frame labels")
+data <- data[tip.order, , drop = FALSE]
+rownames(data) <- data$taxa.nam
+}
