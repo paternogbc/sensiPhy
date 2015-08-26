13762c14a92bdaf28bbf1591a5defd9514450178
Gustavo B Paterno <paternogbc@gmail.com>
2015-08-26 18:03:15 -0300

    last check ok!

--- R/plot_clade_phylolm.R
+++ R/plot_clade_phylolm.R
@@ -24,6 +24,10 @@
 #' @export
 
 sensi_plot.sensiClade <- function(x, clade = NULL, ...){
+
+    full.data <- x$data
+    mapp <- c(x$formula[[3]], x$formula[[2]])
+    clade.col <- x$clade.col
     
     clades.names <- x$clade.model.estimates$clade
     if (is.null(clade) == T){
@@ -33,49 +37,43 @@ sensi_plot.sensiClade <- function(x, clade = NULL, ...){
                 Use clade = 'clade name' to plot results for other clades")
     }
     clade.n <- which(clade == clades.names)
-    if (length(clade.n) == 0) stop(paste(clade,"is not a valide clade name"))
+    if (length(clade.n) == 0) stop(paste(clade,"is not a valid clade name"))
     
     ### Organizing values:
     result <- x$clade.model.estimates
-    vars   <- all.vars(x$formula)
-    vars2  <- gsub("list","",attr(stats::terms(x$formula),"variables"))[-1]
     intercept.0 <-  as.numeric(x$full.model.estimates$coef[1])
     slope.0     <-  as.numeric(x$full.model.estimates$coef[2])
-    cutoff      <-  x$cutoff
-    ### Removing clades with error:
-    if (isTRUE(class(x$errors) != "character" )){
-        x$data <- x$data[-as.numeric(x$errors),]
-    }
-    
+
     inter <- c(x$clade.model.estimates$intercept[clade.n ],
-               x$full.model.estimates$coef[1,1])
+               intercept.0)
     slo <-  c(x$clade.model.estimates$slope[clade.n ],
-              x$full.model.estimates$coef[2,1])
+              slope.0)
     model <- NULL
     estimates <- data.frame(inter,slo, model=c("Without clade","Full model"))
     
-    result.tab <- data.frame(x$data[vars])
-    ggplot2::ggplot(x$data,aes(x=eval(parse(text=vars2[2])),
-                               y=eval(parse(text=vars2[1]))),
-                    environment = environment())+
-        geom_point(data=x$data[x$data$fam!=clade,],alpha=.7,
-                   size=4)+
-        geom_point(data=x$data[x$data$fam==clade,],alpha=.5,
-                   size=4,aes(shape="Removed species"),colour="red")+
-        geom_abline(data=estimates,aes(intercept=inter, slope=slo,
-                                       linetype=factor(model)),
+    match.y <- which(full.data[, clade.col] == clade)
+    match.n <- which(full.data[, clade.col] != clade)
+    
+    g1 <- ggplot2::ggplot(full.data, aes_string(x = mapp[1], y = mapp[2]),
+                    environment = parent.frame())+
+        
+        geom_point(data = full.data[match.n, ], alpha = .7,
+                   size = 4)+
+        geom_point(data = full.data[match.y, ],alpha = .5,
+                   size = 4, aes(shape = "Removed species"), colour = "red")+
+        geom_abline(data = estimates, aes(intercept = inter, slope = slo,
+                                       linetype = factor(model)),
                     size=.8, show_guide = T)+
-        scale_shape_manual(name="",values=c("Removed species"=16))+
+        scale_shape_manual(name = "", values = c("Removed species" = 16))+
         guides(shape = guide_legend(override.aes = list(linetype = 0)))+
-        scale_linetype_manual(name="model",values=c("Full model"="solid",
-                                                    "Without clade"="dashed"))+
+        scale_linetype_manual(name = "Model", values = c("Full model" = "solid",
+                                                    "Without clade" = "dashed"))+
         theme(axis.text = element_text(size = 18),
               axis.title = element_text(size = 18),
               legend.text = element_text(size = 16),
               plot.title = element_text(size = 20),
-              panel.background=element_rect(fill="white",colour="black"))+
-        ylab(vars2[1])+
-        xlab(vars2[2])+
-        ggtitle(paste("Clade removed: ", clade,sep=""))
+              panel.background = element_rect(fill = "white", colour = "black"))+
+        ggtitle(paste("Clade removed: ", clade, sep = ""))
+    return(g1)
 }
 
