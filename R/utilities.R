1e478f1fa0dc4855b05424e4b81b97f792ed490a
Gustavo B Paterno <paternogbc@gmail.com>
2015-08-26 19:16:27 -0300

    fixed problems. Make NA removal simpler (with stats::na.exclude) in model.frame. Other minor details.

--- R/utilities.R
+++ R/utilities.R
@@ -1,53 +1,52 @@
 ### Function to match data and phylogeny:
-match_dataphy <- function(formula,data,phy){
+match_dataphy <- function(formula, data, phy){
 
 # original data set:
 data.0 <- data
 # Croping data frame by formula variables:
-mf <- stats::model.frame(formula = formula, data = data, na.action = stats::na.pass )
+mf <- stats::model.frame(formula = formula, data = data.0, na.action = stats::na.exclude)
+if (nrow(data.0) > nrow(mf)) warning("NA's in response or predictor,", 
+                                     " rows with NA's were removed")
+
 vars <- all.vars(formula)
   
-#Remove NA's before matching data and tips
-if (sum(is.na(mf[,1]))!=0 || sum(is.na(mf[,2]))!=0)
-{data <- mf[!is.na(mf[,1]) & !is.na(mf[,2]),]
- warning("NA's in response or predictor, rows with NA's were removed")}
-
 #Match data and phylogeny in comparative.data style
 if(inherits(phy, "multiPhylo")){  
-  phy1<-phy[[1]]}
+  phy1 <- phy[[1]]}
 else
   phy1<-phy
 
-tiplabl<-phy1$tip.label
-taxa.nam<-as.character(rownames(data))
+tiplabl <- phy1$tip.label
+taxa.nam <- as.character(rownames(mf))
 
 in.both <- intersect(taxa.nam, tiplabl)
 
 if (length(in.both) == 0)
   stop("No tips are common to the dataset and phylogeny")
 
-mismatch<-union(setdiff(tiplabl,taxa.nam),setdiff(taxa.nam,tiplabl))
-if (length(mismatch) != 0)   warning("Phylogeny tips do not match the species list,
-species were dropped from phylogeny or species list")
+mismatch <- union(setdiff(tiplabl,taxa.nam),setdiff(taxa.nam,tiplabl))
+if (length(mismatch) != 0)   warning("Some phylogeny tips do not match species in data,",
+                                     "species were dropped from phylogeny",
+                                     " or data")
 
 #Drop species from tree
 if(inherits(phy, "multiPhylo")){ 
-    phy<-lapply(phy,ape::drop.tip,tip=mismatch)
+    phy <- lapply(phy, ape::drop.tip,tip = mismatch)
     class(phy)<-"multiPhylo"
-    tip.order <- match(phy[[1]]$tip.label, rownames(data))
+    tip.order <- match(phy[[1]]$tip.label, rownames(mf))
 }
 if(inherits(phy, "phylo")){ 
-    phy<- ape::drop.tip(phy,tip=mismatch)
+    phy <- ape::drop.tip(phy,tip = mismatch)
     class(phy)<-"phylo"
-    tip.order <- match(phy$tip.label, rownames(data))
+    tip.order <- match(phy$tip.label, rownames(mf))
 }
 
 if (any(is.na(tip.order)))
     stop("Problem with sorting data frame: mismatch between tip labels and data frame labels")
-data <- data[tip.order, , drop = FALSE]
-data.out <- data.0[rownames(data),]
+data.out <- mf
 
-return(list(data = data.out,phy = phy))
+message(paste("Final dataset with ",nrow(data.out)," species in data and phylogeny"))
+return(list(data = data.out, phy = phy))
 }
 
 ### Function to plot multiple ggplo2 graphs:
