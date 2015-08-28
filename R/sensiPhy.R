5d80825c4ba4892809dfbb8ec53a28de4517ec38
Gustavo B Paterno <paternogbc@gmail.com>
2015-08-28 18:14:56 -0300

    first commit: package documentation.

---
+++ R/sensiPhy.R
@@ -0,0 +1,20 @@
+#' Sensitivity Analysis for Comparative Methods
+#' 
+#' The functions implemented in \bold{sensiPhy} provide diagnostics for phylogenetic 
+#' generalized least squares by various simulations methods: data removal, 
+#' leave-one-out deletion, random sampling of phylogenies and data. 
+#' A graphical output with several plots for model diagnostic is provided 
+#' for each function in the package.
+#'
+#' @name sensiPhy
+#' @author Gustavo Paterno; Caterina Penoni & Gijsbert D.A. Werner
+#' @section Bug reports: 
+#' 
+#' \url{https://github.com/paternogbc/sensiPhy/issues}.
+#' @section How to contribute?: 
+#' 
+#' You can find instructions to contribute with \bold{sensiPhy} at
+#' this link: add link!
+#' @docType package
+NULL
+
