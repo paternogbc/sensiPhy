d5d0ec0c1dade6bae390c7007c763f1a19ec50b8
Gustavo Paterno <paternogbc@gmail.com>
2015-11-16 16:16:13 -0300

    minor... space correction

--- R/intra_phylolm.R
+++ R/intra_phylolm.R
@@ -67,71 +67,73 @@
 #' @export
 
 
-intra_phylm <- function(formula,data,phy,
-                        Vy=NULL,Vx=NULL,
-                        times=2,distrib="normal",
-                        model="lambda",track=TRUE,...){
+intra_phylm <- function(formula, data, phy,
+                        Vy = NULL, Vx = NULL,
+                        times = 2, distrib = "normal",
+                        model = "lambda", track = TRUE, ...){
   #Error check
-  if(class(formula)!="formula") stop("formula must be class 'formula'")
-  if(class(data)!="data.frame") stop("data must be class 'data.frame'")
-  if(class(phy)!="phylo") stop("phy must be class 'phylo'")
-  if(distrib=="normal") warning ("distrib=normal: make sure that standard deviation 
+  if(class(formula) != "formula") stop("formula must be class 'formula'")
+  if(class(data) != "data.frame") stop("data must be class 'data.frame'")
+  if(class(phy) != "phylo") stop("phy must be class 'phylo'")
+  if(distrib == "normal") warning ("distrib=normal: make sure that standard deviation 
                                  is provided for Vx or Vy")
-  else
-    
+
   #Matching tree and phylogeny using utils.R
-  datphy<-match_dataphy(formula,data,phy)
-  full.data<-datphy[[1]]
-  phy<-datphy[[2]]
+  datphy <- match_dataphy(formula, data, phy)
+  full.data <- datphy[[1]]
+  phy <- datphy[[2]]
   
-  resp<-all.vars(formula)[1]
-  pred<-all.vars(formula)[2]
+  resp <- all.vars(formula)[1]
+  pred <- all.vars(formula)[2]
   
-  if(!is.null(Vy) && sum(is.na(full.data[,Vy]))!=0){
-    full.data[is.na(full.data[,Vy]),] <- 0}
+  if(!is.null(Vy) && sum(is.na(full.data[, Vy])) != 0) {
+    full.data[is.na(full.data[, Vy]), ] <- 0}
   
-  if(!is.null(Vx) && sum(is.na(full.data[,Vx]))!=0){
-    full.data[is.na(full.data[,Vx]),] <- 0}
+  if(!is.null(Vx) && sum(is.na(full.data[, Vx])) != 0) {
+    full.data[is.na(full.data[, Vx]), ] <- 0}
   
   
   
   #Function to pick a random value in the interval
-  if (distrib=="normal") funr <- function(a,b) {stats::rnorm(1,a,b)}
-  else  funr <- function(a,b) {stats::runif(1,a-b,a+b)}
+  if (distrib == "normal") funr <- function(a,b) {stats::rnorm(1,a,b)}
+  else  funr <- function(a,b) {stats::runif(1, a - b, a + b)}
   
   #Create the results data.frame
-  intra.model.estimates<-data.frame("n.intra"=numeric(),"intercept"=numeric(),"se.intercept"=numeric(),
-                                    "pval.intercept"=numeric(),"slope"=numeric(),"se.slope"=numeric(),
-                                    "pval.slope"=numeric(),"aic"=numeric(),"optpar"=numeric())
-  
-  
+  intra.model.estimates <- data.frame("n.intra" = numeric(),"intercept" = numeric(),
+                                    "se.intercept" = numeric(), 
+                                    "pval.intercept" = numeric(),
+                                    "slope" = numeric(),
+                                    "se.slope" = numeric(),
+                                    "pval.slope" = numeric(),"aic" = numeric(),
+                                    "optpar" = numeric())
   #Model calculation
-  counter=1
+  counter = 1
   errors <- NULL
-  c.data<-list()
+  c.data <- list()
   
   for (i in 1:times) {
-    
     ##Set response and predictor variables
     #Vy is not provided or is not numeric, do not pick random value
-    if(!inherits(full.data[,resp], c("numeric","integer")) || is.null(Vy)) {full.data$respV<-full.data[,resp]}
+    if(!inherits(full.data[,resp], c("numeric","integer")) || is.null(Vy)) 
+        {full.data$respV <- model.frame(formula, data = full.data)[,1]}
     
     #choose a random value in [mean-se,mean+se] if Vy is provided
-    if(!is.null(Vy))
-    {full.data$respV<-apply(full.data[,c(resp,Vy)],1,function(x)funr(x[1],x[2]))}
-    
+    if (!is.null(Vy))
+    {full.data$respV <- apply(full.data[,c(resp,Vy)],1,function(x)funr(x[1],x[2]))}
     
     #Vx is not provided or is not numeric, do not pick random value
-    if(!inherits(full.data[,pred], c("numeric","integer")) || is.null(Vx)) {full.data$predV<-full.data[,pred]}
+    if (!inherits(full.data[,pred], c("numeric","integer")) || is.null(Vx))
+        {full.data$predV <- model.frame(formula, data = full.data)[,2]}
     
     #choose a random value in [mean-se,mean+se] if Vx is provided
     if(!is.null(Vx))
-    {full.data$predV<-apply(full.data[,c(pred,Vx)],1,function(x)funr(x[1],x[2]))}
+    {full.data$predV <- apply(full.data[,c(pred,Vx)],1,function(x)funr(x[1],x[2]))}
     
     #model
-    mod = try(phylolm::phylolm(respV~predV, data=full.data, model=model,phy=phy),FALSE)
+    mod = try(phylolm::phylolm(respV ~ predV, data = full.data, model = model,
+                               phy = phy), FALSE)
     
-    if(isTRUE(class(mod)=="try-error")) {
+    if(isTRUE(class(mod) == "try-error")) {
       error <- i
       names(error) <- rownames(c.data$full.data)[i]
       errors <- c(errors,error)
@@ -147,17 +149,14 @@ intra_phylm <- function(formula,data,phy,
       pval.slope           <- phylolm::summary.phylolm(mod)$coefficients[[2,4]]
       aic.mod              <- mod$aic
       n                    <- mod$n
-      #d                    <- mod$d
 
-      if (model == "BM"){
+      if (model == "BM") {
         optpar <- NA
       }
-      if (model != "BM"){
-        optpar               <- mod$optpar
+      if (model != "BM") {
+        optpar <- mod$optpar
       }
-      
-      if(track==TRUE) cat("\r","Simu = ", i," / ", times)
-      
+      if(track == TRUE) cat("\r","Simu = ", i," / ", times)
       #write in a table
       estim.simu <- data.frame(i, intercept, se.intercept, pval.intercept,
                                slope, se.slope, pval.slope, aic.mod, optpar,
@@ -170,20 +169,21 @@ intra_phylm <- function(formula,data,phy,
   
   #calculate mean and sd for each parameter
   #variation due to intraspecific variability
-  mean_by_randomval<-stats::aggregate(.~n.intra, data=intra.model.estimates, mean)
+  mean_by_randomval <- stats::aggregate(.~n.intra, data = intra.model.estimates,
+                                        mean)
   
-  statresults<-data.frame(min=apply(intra.model.estimates,2,min),
-                          max=apply(intra.model.estimates,2,max),
-                          mean=apply(intra.model.estimates,2,mean),
-                          sd_intra=apply(mean_by_randomval,2,stats::sd))[-1,]
+  statresults <- data.frame(min = apply(intra.model.estimates, 2, min),
+                          max = apply(intra.model.estimates, 2, max),
+                          mean = apply(intra.model.estimates, 2, mean),
+                          sd_intra = apply(mean_by_randomval, 2, stats::sd))[-1, ]
   
   statresults$CI_low  <- statresults$mean - qt(0.975, df = times-1) * statresults$sd_intra / sqrt(times)
   statresults$CI_high <- statresults$mean + qt(0.975, df = times-1) * statresults$sd_intra / sqrt(times)
   
-  res <- list(formula=formula,
-              datas=full.data,
-              model_results=intra.model.estimates,N.obs=n,
-              stats=statresults)
+  res <- list(formula = formula,
+              datas = full.data,
+              model_results = intra.model.estimates, N.obs = n,
+              stats = statresults)
   class(res) <- "sensiIntra"
   return(res)
 }
