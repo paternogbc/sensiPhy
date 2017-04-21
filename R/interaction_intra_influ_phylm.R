e025f9a5658f34b890f15f38206705f59f168e14
Gijsbert Werner <gijsbertwerner@Gijsberts-MacBook-Pro.local>
2017-04-21 11:39:23 +0200

    Basic idea set up of two nested intra/influ loops, based on the already existing functions. finetuning and testing still needed.

--- R/interaction_intra_influ_phylm.R
+++ R/interaction_intra_influ_phylm.R
@@ -78,7 +78,20 @@
 #'sensi_plot(influ, param = "slope", graphs = 2)
 #' @export
 
-influ_phylm <- function(formula,data,phy,model="lambda",cutoff=2,track=TRUE,...){
+influ_phylm <- function(formula,data,phy,model="lambda",cutoff=2,Vy = NULL, Vx = NULL,
+                        y.transf = NULL, x.transf = NULL,
+                        times = 10, distrib = "normal",
+                        model = "lambda",track=TRUE,...){
+  
+  #Error check
+  if(is.null(Vx) & is.null(Vy)) stop("Vx or Vy must be defined")
+  if(class(formula) != "formula") stop("formula must be class 'formula'")
+  if(class(data) != "data.frame") stop("data must be class 'data.frame'")
+  if(class(phy) != "phylo") stop("phy must be class 'phylo'")
+  if(formula[[2]]!=all.vars(formula)[1] || formula[[3]]!=all.vars(formula)[2])
+    stop("Please use arguments y.transf or x.transf for data transformation")
+  if(distrib == "normal") warning ("distrib=normal: make sure that standard deviation 
+                                   is provided for Vx and/or Vy")
   if(class(formula)!="formula") stop("formula must be class 'formula'")
   if(class(data)!="data.frame") stop("data must be class 'data.frame'")
   if(class(phy)!="phylo") stop("phy must be class 'phylo'")
@@ -97,6 +110,21 @@ influ_phylm <- function(formula,data,phy,model="lambda",cutoff=2,track=TRUE,...)
   pval.intercept.0 <- phylolm::summary.phylolm(mod.0)$coefficients[[1,4]]
   pval.slope.0     <- phylolm::summary.phylolm(mod.0)$coefficients[[2,4]]
   optpar.0 <- mod.0$optpar
+  total_iteration <- N * times #I.e. how often are we going resimulate the dataset, times the # of species to drop. 
+  
+  #To loop over iterations of the dataset based on intraspecific variation. 
+  resp <- all.vars(formula)[1]
+  pred <- all.vars(formula)[2]
+  
+  if(!is.null(Vy) && sum(is.na(full.data[, Vy])) != 0) {
+    full.data[is.na(full.data[, Vy]), Vy] <- 0}
+  
+  if(!is.null(Vx) && sum(is.na(full.data[, Vx])) != 0) {
+    full.data[is.na(full.data[, Vx]), Vx] <- 0}
+  
+  #Function to pick a random value in the interval
+  if (distrib == "normal") funr <- function(a,b) {stats::rnorm(1,a,b)}
+  else  funr <- function(a,b) {stats::runif(1, a - b, a + b)}
   
   
   #Creates empty data frame to store model outputs
@@ -108,51 +136,88 @@ influ_phylm <- function(formula,data,phy,model="lambda",cutoff=2,track=TRUE,...)
                "pval.slope"=numeric(),"AIC"=numeric(),
                "optpar" = numeric())
   
+
   #Loops over all species, and removes each one individually
   counter <- 1
   errors <- NULL
-  pb <- utils::txtProgressBar(min = 0, max = N, style = 1)
-  for (i in 1:N){
-    
-    crop.data <- full.data[c(1:N)[-i],]
-    crop.phy <-  ape::drop.tip(phy,phy$tip.label[i])
-    mod=try(phylolm::phylolm(formula, data=crop.data,model=model,
-                             phy=crop.phy),
-            TRUE)
-    if(isTRUE(class(mod)=="try-error")) {
-      error <- i
-      names(error) <- rownames(full.data$data)[i]
-      errors <- c(errors,error)
-      next }
-    else {  sp                   <- phy$tip.label[i]
-    intercept            <- mod$coefficients[[1]]
-    slope                <- mod$coefficients[[2]]
-    DFintercept          <- intercept - intercept.0
-    DFslope              <- slope - slope.0
-    intercept.perc       <- round((abs(DFintercept/intercept.0))*100,digits=1)
-    slope.perc           <- round((abs(DFslope/slope.0))*100,digits=1)
-    pval.intercept       <- phylolm::summary.phylolm(mod)$coefficients[[1,4]]
-    pval.slope           <- phylolm::summary.phylolm(mod)$coefficients[[2,4]]
-    aic.mod              <- mod$aic
-    if (model == "BM" | model == "trend"){
-      optpar <- NA
-    }
-    if (model != "BM" & model != "trend" ){
-      optpar               <- mod$optpar
-    }
-    
-    if(track==TRUE)
-      utils::setTxtProgressBar(pb, i)
-    # Stores values for each simulation
-    estim.simu <- data.frame(sp, intercept, DFintercept, intercept.perc,
-                             pval.intercept, slope, DFslope, slope.perc,
-                             pval.slope, aic.mod, optpar,
-                             stringsAsFactors = F)
-    influ.model.estimates[counter, ]  <- estim.simu
-    counter=counter+1
+  pb <- utils::txtProgressBar(min = 0, max = total_iteration, style = 1)
+  for (j in 1:total_iteration){ #Create a nested for-loop. 
+    for (i in 1:times) { #First create the new datset, and then drop all the species on that as previously. 
+      ##Set response and predictor variables
+      #Vy is not provided or is not numeric, do not pick random value
+      if(!inherits(full.data[,resp], c("numeric","integer")) || is.null(Vy)) 
+      {full.data$respV <- stats::model.frame(formula, data = full.data)[,1]}
+      
+      #choose a random value in [mean-se,mean+se] if Vy is provided
+      if (!is.null(Vy))
+      {full.data$respV <- apply(full.data[,c(resp,Vy)],1,function(x)funr(x[1],x[2]))}
+      
+      #Vx is not provided or is not numeric, do not pick random value
+      if (!inherits(full.data[,pred], c("numeric","integer")) || is.null(Vx))
+      {full.data$predV <- stats::model.frame(formula, data = full.data)[,2]}
+      
+      #choose a random value in [mean-se,mean+se] if Vx is provided
+      if(!is.null(Vx))
+      {full.data$predV <- apply(full.data[,c(pred,Vx)],1,function(x)funr(x[1],x[2]))}
+      
+      #transform Vy and/or Vx if x.transf and/or y.transf are provided
+      if(!is.null(y.transf)) 
+      {suppressWarnings (full.data$respV <- y.transf(full.data$respV))}
+      
+      if(!is.null(x.transf)) 
+      {suppressWarnings (full.data$predV <- x.transf(full.data$predV))}
+      
+      #skip iteration if there are NA's in the dataset
+      species.NA[[i]]<-rownames(full.data[with(full.data,is.na(predV) | is.na(respV)),])
+      if(sum(is.na(full.data[,c("respV","predV")])>0)) next
+      
+      #Here, go into the species-drop loop:
+      for (k in 1:N){
+      crop.data <- full.data[c(1:N)[-k],]
+      crop.phy <-  ape::drop.tip(phy,phy$tip.label[k])
+      mod=try(phylolm::phylolm(formula, data=crop.data,model=model,
+                               phy=crop.phy),
+              TRUE)
+      if(isTRUE(class(mod)=="try-error")) {
+        error <- k
+        names(error) <- rownames(full.data$data)[k]
+        errors <- c(errors,error)
+        next }
+      else {  sp                   <- phy$tip.label[k]
+      intercept            <- mod$coefficients[[1]]
+      slope                <- mod$coefficients[[2]]
+      DFintercept          <- intercept - intercept.0
+      DFslope              <- slope - slope.0
+      intercept.perc       <- round((abs(DFintercept/intercept.0))*100,digits=1)
+      slope.perc           <- round((abs(DFslope/slope.0))*100,digits=1)
+      pval.intercept       <- phylolm::summary.phylolm(mod)$coefficients[[1,4]]
+      pval.slope           <- phylolm::summary.phylolm(mod)$coefficients[[2,4]]
+      aic.mod              <- mod$aic
+      if (model == "BM" | model == "trend"){
+        optpar <- NA
+      }
+      if (model != "BM" & model != "trend" ){
+        optpar               <- mod$optpar
+      }
+      
+      if(track==TRUE)
+        utils::setTxtProgressBar(pb, k)
+      # Stores values for each simulation
+      estim.simu <- data.frame(sp, intercept, DFintercept, intercept.perc,
+                               pval.intercept, slope, DFslope, slope.perc,
+                               pval.slope, aic.mod, optpar,
+                               stringsAsFactors = F)
+      influ.model.estimates[counter, ]  <- estim.simu
+      counter=counter+1
+      }
+      }
     }
   }
+   
+
   on.exit(close(pb))
+  
+  
   #Calculates Standardized DFbeta and DFintercept
   sDFintercept <- influ.model.estimates$DFintercept/
     stats::sd(influ.model.estimates$DFintercept)
