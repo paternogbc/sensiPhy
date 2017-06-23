c6a68631ed446076b68bf0cbf2de7376bf75143d
Gijsbert Werner <gijsbertwerner@Gijsberts-MacBook-Pro.local>
2017-06-23 16:54:43 +0200

    Add help function.

--- R/interaction_intra_influ_phylm.R
+++ R/interaction_intra_influ_phylm.R
@@ -1,3 +1,81 @@
+#' Interaction of intraspecific variability & influential species - Phylogenetic Linear Regression
+#'
+#' Performs leave-one-out deletion analyis for phylogenetic linear regression,
+#' and detects influential species, while taking into account potential
+#' interactions with intraspecific variability.
+#'
+#' @param formula The model formula:  \code{response~predictor}. 
+#' @param data Data frame containing species traits with row names matching tips
+#' in \code{phy}.
+#' @param phy A phylogeny (class 'phylo') matching \code{data}.
+#' @param model The phylogenetic model to use (see Details). Default is \code{lambda}.
+#' @param cutoff The cutoff value used to identify for influential species
+#' (see Details)
+#' @param n.intra Number of datasets resimulated taking into account intraspecific variation (see: \code{"intra_phylm"})
+#' @param Vy Name of the column containing the standard deviation or the standard error of the response 
+#' variable. When information is not available for one taxon, the value can be 0 or \code{NA}.
+#' @param Vx Name of the column containing the standard deviation or the standard error of the predictor 
+#' variable. When information is not available for one taxon, the value can be 0 or \code{NA}
+#' @param y.transf Transformation for the response variable (e.g. \code{"log"} or \code{"sqrt"}). Please use this 
+#' argument instead of transforming data in the formula directly (see also details below).
+#' @param x.transf Transformation for the predictor variable (e.g. \code{"log"} or \code{"sqrt"}). Please use this 
+#' argument instead of transforming data in the formula directly (see also details below).
+#' @param distrib A character string indicating which distribution to use to generate a random value for the response 
+#' and/or predictor variables. Default is normal distribution: "normal" (function \code{\link{rnorm}}).
+#' Uniform distribution: "uniform" (\code{\link{runif}})
+#' Warning: we recommend to use normal distribution with Vx or Vy = standard deviation of the mean.
+#' @param track Print a report tracking function progress (default = TRUE)
+#' @param ... Further arguments to be passed to \code{phylolm}
+#' @details
+#' This function fits a phylogenetic linear regression model using \code{\link[phylolm]{phylolm}}, and detects
+#' influential species by sequentially deleting one at a time. The regression is repeated \code{n.intra} times for 
+#' simulated values of the dataset, taking into account intraspecific variation. At each iteration, the function 
+#' generates a random value for each row in the dataset using the standard deviation or errors supplied, and 
+#' detect the influential species within that iteration. 
+#'
+#' All phylogenetic models from \code{phylolm} can be used, i.e. \code{BM},
+#' \code{OUfixedRoot}, \code{OUrandomRoot}, \code{lambda}, \code{kappa},
+#' \code{delta}, \code{EB} and \code{trend}. See ?\code{phylolm} for details.
+#'
+#' \code{influ_phylm} detects influential species based on the standardised
+#' difference in intercept and/or slope when removing a given species compared
+#' to the full model including all species. Species with a standardised difference
+#' above the value of \code{cutoff} are identified as influential. The default
+#' value for the cutoff is 2 standardised differences change.
+#'
+#' Currently, this function can only implement simple linear models (i.e. \eqn{trait~
+#' predictor}). In the future we will implement more complex models.
+#'
+#' Output can be visualised using \code{sensi_plot}.
+#' 
+#' @section Warning:  
+#' When Vy or Vx exceed Y or X, respectively, negative (or null) values can be generated, this might cause problems
+#' for data transformation (e.g. log-transformation). In these cases, the function will skip the simulation. 
+#' 
+#' Setting \code{n.intra} at high values can take a long time to exectue, since the total number of iterations 
+#' equals \code{n.intra * nrow(data)}.
+#'
+#' @author Gustavo Paterno, Caterina Penone & Gijsbert D.A. Werner
+#' @seealso \code{\link[phylolm]{phylolm}}, \code{\link{samp_phylm}},
+#' \code{\link{influ_phylm}},\code{\link{intra_phylm}},\code{\link{sensi_plot}}.
+#' @references Ho, L. S. T. and Ane, C. 2014. "A linear-time algorithm for 
+#' Gaussian and non-Gaussian trait evolution models". Systematic Biology 63(3):397-408.
+#' @examples 
+#' # Load data:
+#' data(alien)
+#' # run analysis:
+#' intra_influ <- interaction_intra_influ_phylm(formula = gestaLen ~ adultMass, phy = alien$phy[[1]],
+#' data=alien$data,model="lambda",y.transf = "log",x.transf = NULL,Vy="SD_gesta",Vx=NULL,
+#' n.intra=10,distrib = "normal")
+#' # To check summary results:
+#'summary(intra_influ)
+#'# Most influential speciesL
+#'intra_influ$influential.species
+#'# Visual diagnostics
+#'sensi_plot(intra_influ)
+#'# You can specify which graph and parameter ("slope" or "intercept") to print: 
+#'sensi_plot(intra_influ, param = "slope", graphs = 2)
+#' @export
 
 interaction_intra_influ_phylm <- function(formula, data, phy,
                         Vy = NULL, Vx = NULL,
