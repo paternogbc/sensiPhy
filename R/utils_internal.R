bee4d766f92bd40bb1f15fc341db40eca15c7343
caterinap <caterina.penone@gmail.com>
2017-08-25 17:57:00 +0200

    Modify recombine Now it should handle cases where some models did not work (i.e. datasets in list have different dimensions). It also add row.names when necessary

--- R/utils_internal.R
+++ R/utils_internal.R
@@ -3,7 +3,6 @@
 recombine <- function(list, slot1, slot2 = NULL){
   ### One level indexing list[[slot1]]:
   if (is.null(slot2)){
-    nam <- as.numeric(names(list))
     x <- lapply(list, function(x) x[[slot1]])
     
     if(is(x[[1]],"list"))
@@ -11,15 +10,13 @@ recombine <- function(list, slot1, slot2 = NULL){
     if(is.na(match(class(x[[1]]), c("data.frame", "matrix"))))
       stop("Check if slot indexing returns a valid data.frame or numeric value withn your list")
     
-    n.rows<- nrow(x[[1]])
-    N <- rep(nam, each = n.rows)
-    res <- data.frame(iteration = N, do.call("rbind", x))
+    res <- data.frame(iteration=rep(names(x), sapply(x, nrow)), do.call(rbind, x))
+    res$info <- unlist(lapply(x,rownames))
     return(res)
   }
   
   ### Two levels indixing list[[slot1]][[slot2]]
   if(is.null(slot2) == FALSE){ 
-    nam <- as.numeric(names(list))
     x <- lapply(list, function(x) x[[slot1]][[slot2]])
     
     if(class(x[[1]]) == "data.frame" | class(x[[1]]) == "matrix" | class(x[[1]]) == "numeric" ){
@@ -27,6 +24,7 @@ recombine <- function(list, slot1, slot2 = NULL){
       
       ### If there is only a single value (e.g. AIC, optpar)
       if(is.null(n.rows)) {
+        nam <- as.numeric(names(list))
         n.rows <- length(x[[1]])
         est.name <- names(list[[1]][[slot1]])[[slot2]]
         N <- rep(nam, each = n.rows)
@@ -36,8 +34,8 @@ recombine <- function(list, slot1, slot2 = NULL){
       }
       
       else
-        N <- rep(nam, each = n.rows)
-      res <- data.frame(iteration = N, do.call("rbind", x))
+      res <- data.frame(iteration=rep(names(x), sapply(x, nrow)), do.call(rbind, x))
+      res$info <- unlist(lapply(x,rownames))
       return(res)
     }
     else stop("Check if slot indexing returns a valid data.frame or numeric value withn your list") 
