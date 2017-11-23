fdd6add79b756f442ddb1032081bc88faaf408a1
Gijsbert Werner <gijsbertwerner@Gijsberts-MacBook-Pro.local>
2017-11-23 17:47:41 +0000

    Full help for influ_Discrete

--- R/influ_Discrete.R
+++ R/influ_Discrete.R
@@ -1,3 +1,59 @@
+#' Influential Species Detection - Trait Evolution Discrete Characters
+#' 
+#' Fits models for trait evolution of discrete (binary) characters, 
+#' detecting influential species. 
+#'
+#' @param data Data vector for a single binary trait, with names matching tips in \code{phy}.
+#' @param phy A phylogeny (class 'phylo') matching \code{data}.
+#' @param model The Mkn model to use (see Details). 
+#' @param transform The evolutionary model to transform the tree (see Details). Default is \code{none}.
+#' @param bounds settings to contstrain parameter estimates. See \code{\link[geiger]{fitDiscrete}}
+#' @param track Print a report tracking function progress (default = TRUE)
+#' @param ... Further arguments to be passed to \code{\link[geiger]{fitDiscrete}}
+#' @details
+#' This function sequentially removes one species at a time,  
+#' fits different models of discrete character evolution using \code{\link[geiger]{fitDiscrete}}, 
+#' repeats this this many times (controlled by \code{n.sim}), stores the results and calculates 
+#' the effects on model parameters Currently, only binary discrete traits are supported.
+#' 
+#' #' \code{influ_discrete} detects influential species based on the standardised
+#' difference in q12 or q21 when removing a given species compared
+#' to the full model including all species. Species with a standardised difference
+#' above the value of \code{cutoff} are identified as influential. 
+#' 
+#' Different character model from \code{fitDiscrete} can be used, including \code{ER} (equal-rates), 
+#' \code{SYM} (symmetric), \code{ARD} (all-rates-different) and \code{meristic} (stepwise fashion). 
+#'
+#' All transformations to the phylogenetic tree from \code{fitDiscrete} can be used, i.e. \code{none},
+#' \code{EB}, \code{lambda}, \code{kappa} and\code{delta}.
+#' 
+#' See \code{\link[geiger]{fitDiscrete}} for more details on character models and tree transformations. 
+#' 
+#' Output can be visualised using \code{sensi_plot}. [Not yet!]
+#'
+#' @return The function \code{tree_discrete} returns a list with the following
+#' components:
+#' @return \code{call}: The function call
+#' @return \code{cutoff}: The value selected for \code{cutoff}
+#' @return \code{data}: The original full data vector
+#' @return \code{optpar}: Transformation parameter used (e.g. \code{lambda}, \code{kappa} etc.)
+#' @return \code{full.model.estimates}: Parameter estimates (transition rates q12 and q21), 
+#' AICc and the optimised value of the phylogenetic transformation parameter (e.g. \code{lambda}) 
+#' for the full model without deleted species.
+#' @return \code{influential_species}: List of influential species, based on standardised 
+#' difference in estimates for q12 and q21. Species are ordered from most influential to 
+#' less influential and only include species with a standardised difference > \code{cutoff}.
+#' @return \code{sensi.estimates}: Parameter estimates (transition rates q12 and q21), 
+#' AICc and the optimised value of the phylogenetic transformation parameter (e.g. \code{lambda}) 
+#' for each analysis with a different phylogenetic tree.
+#' @author Gijsbert Werner & Gustavo Paterno
+#' @seealso \code{\link[geiger]{fitDiscrete}}
+#' @references 
+#' Yang Z. 2006. Computational Molecular Evolution. Oxford University Press: Oxford. 
+#' 
+#' Harmon Luke J, Jason T Weir, Chad D Brock, Richard E Glor, and Wendell Challenger. 2008.
+#' GEIGER: investigating evolutionary radiations. Bioinformatics 24:129-131.
+#' 
 #' @examples 
 #' #Load data:
 #' data("primates")
