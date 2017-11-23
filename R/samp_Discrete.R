e75a3ecfaccea9f5c6c843d1828b35e2bc331680
Gijsbert Werner <gijsbertwerner@Gijsberts-MacBook-Pro.local>
2017-11-23 14:42:21 +0000

    Set up basic function calling + error checking.

--- R/samp_Discrete.R
+++ R/samp_Discrete.R
@@ -1,107 +1,19 @@
-#' Sensitivity Analysis Species Sampling  - Phylogenetic Linear Regression
-#'
-#' Performs analyses of sensitivity to species sampling by randomly removing
-#' species and detecting the effects on parameter estimates in a phylogenetic
-#' linear regression.
-#'
-#' @param formula The model formula
-#' @param data Data frame containing species traits with row names matching tips
-#' in \code{phy}.
-#' @param phy A phylogeny (class 'phylo') matching \code{data}.
-#' @param n.sim The number of times species are randomly deleted for each
-#' \code{break}.
-#' @param breaks A vector containing the percentages of species to remove.
-#' @param model The phylogenetic model to use (see Details). Default is \code{lambda}.
-#' @param track Print a report tracking function progress (default = TRUE)
-#' @param ... Further arguments to be passed to \code{phylolm}
-#' @details
-#'
-#' This function randomly removes a given percentage of species (controlled by
-#' \code{breaks}) from the full phylogenetic linear regression, fits a phylogenetic
-#' linear regression model without these species using \code{\link[phylolm]{phylolm}},
-#' repeats this many times (controlled by \code{n.sim}), stores the results and
-#' calculates the effects on model parameters.
-#'
-#' All phylogenetic models from \code{phylolm} can be used, i.e. \code{BM},
-#' \code{OUfixedRoot}, \code{OUrandomRoot}, \code{lambda}, \code{kappa},
-#' \code{delta}, \code{EB} and \code{trend}. See ?\code{phylolm} for details.
-#'
-#' Currently, this function can only implement simple linear models (i.e. \eqn{trait~
-#' predictor}). In the future we will implement more complex models.
-#'
-#' Output can be visualised using \code{sensi_plot}.
-#' @return The function \code{samp_phylm} returns a list with the following
-#' components:
-#' @return \code{formula}: The formula
-#' @return \code{full.model.estimates}: Coefficients, aic and the optimised
-#' value of the phylogenetic parameter (e.g. \code{lambda} or \code{kappa}) for
-#' the full model without deleted species.
-#' @return \code{sensi.estimates}: A data frame with all simulation
-#' estimates. Each row represents a model rerun with a given number of species
-#' \code{n.remov} removed, representing \code{n.percent} of the full dataset.
-#' Columns report the calculated regression intercept (\code{intercept}),
-#' difference between simulation intercept and full model intercept (\code{DIFintercept}),
-#' the percentage of change in intercept compared to the full model (\code{intercept.perc})
-#' and intercept p-value (\code{pval.intercept}). All these parameters are also reported
-#' for the regression slope (\code{DIFestimate} etc.). Additionally, model aic value
-#' (\code{AIC}) and the optimised value (\code{optpar}) of the phylogenetic
-#' parameter (e.g. \code{kappa} or \code{lambda}, depending on the phylogenetic model
-#' used) are reported. Lastly we reported the standardised difference in intercept 
-#' (\code{sDIFintercept}) and slope (\code{sDIFestimate}). 
-#' @return \code{sign.analysis} For each break (i.e. each percentage of species
-#' removed) this reports the percentage of statistically significant (at p<0.05)
-#' intercepts (\code{perc.sign.intercept}) over all repetitions as well as the
-#' percentage of statisticaly significant (at p<0.05) slopes (\code{perc.sign.estimate}).
-#' @return \code{data}: Original full dataset.
-#' #' @note Please be aware that dropping species may reduce power to detect 
-#' significant slopes/intercepts and may partially be responsible for a potential 
-#' effect of species removal on p-values. Please also consult standardised differences
-#' in the (summary) output.
-#' @author Gustavo Paterno & Gijsbert D.A. Werner
-#' @seealso \code{\link[phylolm]{phylolm}}, \code{\link{samp_phyglm}},
-#' \code{\link{influ_phylm}},\code{\link{sensi_plot}}
-#' @references 
-#' 
-#' Werner, G.D.A., Cornwell, W.K., Sprent, J.I., Kattge, J. & Kiers, E.T. (2014).
-#'  A single evolutionary innovation drives the deep evolution of symbiotic N2-fixation
-#'   in angiosperms. Nature Communications, 5, 4087.
-#'   
-#' Ho, L. S. T. and Ane, C. 2014. "A linear-time algorithm for 
-#' Gaussian and non-Gaussian trait evolution models". Systematic Biology 63(3):397-408.
-#' 
-#' @import ape phylolm
-#' 
-#' @examples 
-#' # Load data:
-#' data(alien)
-#' # Run analysis:
-#' samp <- samp_phylm(log(gestaLen) ~ log(adultMass), phy = alien$phy[[1]], 
-#' data = alien$data, n.sim = 10)
-#' summary(samp)
-#' head(samp$sensi.estimates)
-#' # Visual diagnostics
-#' \dontrun{
-#' sensi_plot(samp)
-#' # You can specify which graph and parameter ("estimate" or "intercept") to print: 
-#' sensi_plot(samp, graphs = 1, param = "estimate")
-#' sensi_plot(samp, graphs = 2, param = "intercept")
-#' }
+
 #' @export
 
-samp_phylm <- function(formula,data,phy,n.sim=30,
-                         breaks=seq(.1,.5,.1),model="lambda",track=TRUE,...){
-# Basic error checking:
-if(class(formula) != "formula") 
-    stop("formula must be class 'formula'")
-if(class(data) != "data.frame") 
-    stop("data must be class 'data.frame'")
-if(class(phy) != "phylo") 
-    stop("phy must be class 'phylo'")
-if(length(breaks) < 2) 
+samp_Discrete <- function(data,phy,n.sim=30,
+                          breaks=seq(.1,.5,.1),
+                          model="ARD",transform="none",
+                          bounds=list(),track=TRUE,...){
+  
+  #Error check
+  if(class(data)!="factor") stop("data must supplied as a factor with species as names. Consider as.factor()")
+  if(length(levels(data))>2) stop("discrete data can have maximal two levels")
+  if(class(phy)!="phylo") stop("phy must be class 'phylo'")
+  if(transform=="white") stop("the white-noise (non-phylogenetic) model is not allowed")
+  if(length(breaks) < 2) 
     stop("Please include more than one break, e.g. breaks=c(.3,.5)")
-if ( (model == "trend") & (ape::is.ultrametric(phy)))
-    stop("Trend is unidentifiable for ultrametric trees., see ?phylolm for details")
-else
+  else
 
 # Check match between data and phy 
 data_phy <- match_dataphy(formula, data, phy, ...)
